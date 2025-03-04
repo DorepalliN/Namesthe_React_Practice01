b0f5eb6930d60b038c61806c255ab0aa179c574a
DorepalliN <nagd329@gmail.com>
2025-03-04 20:59:42 +0530

    practice 01

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-const Heading=<h1 id="head">hello react i am back</h1>
+import react from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading=<h1 className="head">hello react i am back</h1>
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(Heading);
